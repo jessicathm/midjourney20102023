b89ae363c57cb636c4ac47a32bc3600e022ad8a3
jessicathm <147786632+jessicathm@users.noreply.github.com>
2023-10-20 13:09:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,98 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "de87e595-b466-4149-ad73-b6645e120d45",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app '__main__'\n",
-      " * Debug mode: off\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.\n",
-      " * Running on http://127.0.0.1:5000\n",
-      "Press CTRL+C to quit\n"
-     ]
-    }
-   ],
-   "source": [
-    "from flask import Flask\n",
-    "from flask import render_template,request\n",
-    "import json\n",
-    "import time\n",
-    "import requests\n",
-    "\n",
-    "app = Flask(__name__)\n",
-    "\n",
-    "headers = {\n",
-    "    \"Authorization\" : \"Token r8_0AxQUbuX68fMBhYnKFk6whir8HiMxH943p3FC\" ,\n",
-    "    \"Content-Type\" : \"application/json\"\n",
-    "}\n",
-    "\n",
-    "@app.route(\"/\", methods=[\"GET\",\"POST\"])\n",
-    "def index():\n",
-    "    if request.method == \"POST\":\n",
-    "        q = request.form.get(\"question\")\n",
-    "        print(q)\n",
-    "        body = json.dumps(\n",
-    "            {\n",
-    "                \"version\" : \"db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf\",\n",
-    "                \"input\" : {\n",
-    "                    \"prompt\" : q\n",
-    "                }\n",
-    "            }\n",
-    "        )\n",
-    "        output = requests.post(\"https://api.replicate.com/v1/predictions\", data=body, headers=headers)\n",
-    "        time.sleep(10)\n",
-    "        get_url = output.json()[\"urls\"][\"get\"]\n",
-    "        get_result = requests.post(get_url, headers=headers).json()[\"output\"]\n",
-    "        \n",
-    "        \n",
-    "        return(render_template(\"index.html\",result=get_result[0]))\n",
-    "    else:\n",
-    "        return(render_template(\"index.html\",result=\"waiting......\"))\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "2d96efc3-0843-4070-8839-2a8c87e63036",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "anaconda-panel-2023.05-py310",
-   "language": "python",
-   "name": "conda-env-anaconda-panel-2023.05-py310-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
+from flask import Flask
+from flask import render_template,request
+import json
+import time
+import requests
+
+app = Flask(__name__)
+
+headers = {
+    "Authorization" : "Token r8_0AxQUbuX68fMBhYnKFk6whir8HiMxH943p3FC" ,
+    "Content-Type" : "application/json"
 }
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("question")
+        print(q)
+        body = json.dumps(
+            {
+                "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input" : {
+                    "prompt" : q
+                }
+            }
+        )
+        output = requests.post("https://api.replicate.com/v1/predictions", data=body, headers=headers)
+        time.sleep(10)
+        get_url = output.json()["urls"]["get"]
+        get_result = requests.post(get_url, headers=headers).json()["output"]
+        
+        
+        return(render_template("index.html",result=get_result[0]))
+    else:
+        return(render_template("index.html",result="waiting......"))
+
+if __name__ == "__main__":
+    app.run()
